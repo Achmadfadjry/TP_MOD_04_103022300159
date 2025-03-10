ad091bdfbbf6fec5f39f0a03856d07c467f444f5
Achmadfadjry <achmadfadjryadlilfaqih@gmail.com>
2025-03-10 21:38:05 +0700

    menambahkan implementasi dengan table-driven

---
+++ TP_MOD_04_103022300159/KodePos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TP_MOD_04_103022300159
+{
+    class KodePos
+    {
+        private Dictionary<string, int> kodePosTable = new Dictionary<string, int>
+        {
+            { "Batununggal", 40266 },
+            { "Kujangsari", 40287 },
+            { "Mengger", 40267 },
+            { "Wates", 40256 },
+            { "Cijaura", 40287 },
+            { "Jatisari", 40286 },
+            { "Margasari", 40286 },
+            { "Sekejati", 40286 },
+            { "Kebonwaru", 40272 },
+            { "Maleer", 40274 },
+            { "Samoja", 40273 }
+        };
+
+        public int GetKodePos(string inputKelurahan)
+        {
+            if (string.IsNullOrEmpty(inputKelurahan)) return -1;
+
+            inputKelurahan = inputKelurahan.ToLower();
+
+            foreach (var kelurahan in kodePosTable.Keys)
+            {
+                if (kelurahan.ToLower() == inputKelurahan)
+                {
+                    return kodePosTable[kelurahan];
+                }
+            }
+
+            return -1;
+        }
+    }
+}
--- TP_MOD_04_103022300159/Program.cs
+++ TP_MOD_04_103022300159/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP_MOD_04_103022300159;
+
+namespace Tpmod4_103022330075
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            KodePos kodePosObj = new KodePos();
+            
+
+            Console.Write("Masukkan nama kelurahan: ");
+            string input = Console.ReadLine()?.Trim() ?? "";
+
+            int kode = kodePosObj.GetKodePos(input);
+
+            if (kode != -1)
+            {
+                Console.WriteLine($"Kode Pos dari {input} adalah {kode}");
+            }
+            else
+            {
+                Console.WriteLine("Kelurahan tidak ditemukan.");
+            }
+
+            Console.WriteLine("\n===== Simulasi Perubahan State Pintu =====");
+
+
+        }
+    }
+}
